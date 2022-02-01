ecb43c9f734e78ffa7df56f4a10f1749fcf59ac6
maharjann162 <maharjann162@strose.edu>
2022-02-01 15:27:03 -0500

    Update

---
+++ course-api/src/test/java/io/javabrains/springboot/quickstart/courseapi/CourseApiApplicationTests.java
@@ -0,0 +1,13 @@
+package io.javabrains.springboot.quickstart.courseapi;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CourseApiApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
